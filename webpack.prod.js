4966e9972d8976bcc80d2a8b7f3d6376a1c7bb47
Abu MHD <mrabumhd@gmail.com>
2025-02-08 13:18:45 +0100

    Setup script for build, dev, and webpack serve

---
+++ webpack.prod.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // for HTML bundling
+const path = require("path"); // for js bundling
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  // Source map
+  devtool: "eval-source-map",
+  // Webpack Dev Server
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
